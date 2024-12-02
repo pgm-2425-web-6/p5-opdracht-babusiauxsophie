a340e219a205b2d563ee924663fc1b45955c080a
babusiauxsophie <babusiaux.sophie@gmail.com>
2024-12-02 14:47:57 +0100

    wip

--- sketch.js
+++ sketch.js
@@ -19,29 +19,38 @@ function setup() {
 function draw() {
     background("whitesmoke");
 
-    noStroke();
-    fill(ball.color);
-    ellipse(ball.x, ball.y, ball.size);
-
-    let innerText = "Inhale";
+    let elapsedTime = 0;
     if (isAnimating) {
-        let elapsedTime = millis() - animationStartTime;
+        elapsedTime = millis() - animationStartTime;
 
         if (elapsedTime <= 3000) {
             ball.size = lerp(ball.originalSize, 120, elapsedTime / 3000);
             ball.color = lerpColor(color(ball.originalColor), color("#5778ff"), elapsedTime / 3000);
-            innerText = "Inhale";
         } else if (elapsedTime <= 6000) {
             ball.size = lerp(120, ball.originalSize, (elapsedTime - 3000) / 3000);
             ball.color = lerpColor(color("#5778ff"), color(ball.originalColor), (elapsedTime - 3000) / 3000);
-            innerText = "Exhale";
         } else {
             animationStartTime = millis();
         }
     }
 
+    noFill();
+    stroke("whitesmoke");
+    strokeWeight(4);
+    ellipse(ball.x, ball.y, ball.size + 10);
+
+    noStroke();
+    fill(ball.color);
+    ellipse(ball.x, ball.y, ball.size); 
+
+    let innerText = "Inhale";
+    if (elapsedTime > 3000 && elapsedTime <= 6000) {
+        innerText = "Exhale";
+    }
+
+    let textSizeFactor = lerp(15, 25, (ball.size - ball.originalSize) / 70);
     fill("whitesmoke");
-    textSize(18);
+    textSize(textSizeFactor);
     textAlign(CENTER, CENTER);
     text(innerText, ball.x, ball.y);
 
